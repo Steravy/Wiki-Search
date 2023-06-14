3069c54d359fa5bb384920c692414dd1c32bf3a0
Stefan Vitoria <steph.74lw@gmail.com>
2023-06-14 14:29:11 -0100

    created function to fecth data from wikipedia API

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,15 @@
+
+type Props = {
+    params: {
+        searchTerm: string
+    }
+}
+
+export default function SearchResult({ params: { searchTerm } }: Props) {
+
+    return (
+        <div>page</div>
+    )
+}
+
+
